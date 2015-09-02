20cc1bf074a63b378a84e52caf3a2dd77f2ece2c
Stewart Mackenzie <setori88@gmail.com>
2015-09-02 17:26:52 +0800

    urls correctly formatted

--- src/sjmackenzie/boot_nix.clj
+++ src/sjmackenzie/boot_nix.clj
@@ -12,13 +12,20 @@
   "Creates a Nix Expression enumerating all this project's dependencies"
   []
 
-  (def repositories (:repositories (core/get-env)))
+  (defn repo-url [repo-desc] (if (string? repo-desc) repo-desc (:url repo-desc)))
+
+  (def repositories (apply hash-map (flatten (:repositories (core/get-env)))))
+
+  (def orig-repo-map (map (fn [[repo-alias desc]] (vector repo-alias (repo-url desc))) repositories))
+  (def extra-repos ["central" "https://repo1.maven.org/maven2/"])
+  (def repo-map (conj orig-repo-map extra-repos ))
+
   (def resolved-deps
     (boot.pod/with-call-worker
       (boot.aether/resolve-dependencies ~(core/get-env))))
 
   (def repos {:maven "/_maven.repositories"
-                     :remote "/_remote.repositories"})
+              :remote "/_remote.repositories"})
 
   (defn find-repo-file [file-path]
     (let [exists (fn [repo-file] (.exists (io/as-file (str file-path repo-file))))]
@@ -26,25 +33,20 @@
         (exists (:maven repos)) (slurp (str file-path (:maven repos)))
         (exists (:remote repos)) (slurp (str file-path (:remote repos))))))
 
-  #_(defn filter-)
-
   (defn find-repo-server [repo-file-contents]
-    ()
-    (first (filter (fn [[k v]]
-                     (when (re-find (re-pattern k) repo-file-contents) v)) repositories)))
+    (filter string? (map (fn [[k v]] (when (re-find (re-pattern k) repo-file-contents) v)) repo-map)))
 
-  (defn extract-repo [file-path file-name]
+  (defn extract-repo [file-path]
     (let [repo-file-contents (find-repo-file file-path)
-          repo-server (find-repo-server repo-file-contents)
-          ]
-      repo-server)
-    )
+          repo-server (find-repo-server repo-file-contents)]
+      (first repo-server)))
 
   (defn create-url [jar]
     (let [file-path (string/join "/" (butlast (string/split jar #"/")))
-          base-url (str (extract-repo file-path (last (string/split jar #"/"))) )
-          ]
-      base-url))
+          base-path (drop 5 (string/split file-path #"/"))
+          base-url (str (extract-repo file-path) )
+          url (str base-url (string/join "/" base-path) "/" (last (string/split jar #"/")))]
+      url))
 
   (defn destructure-dep [dep]
     (let [name+version (:dep dep)
@@ -57,10 +59,10 @@
       base-url))
 
   (defn format-resolved-deps [deps]
-     (map #(destructure-dep %) deps))
+    (map #(destructure-dep %) deps))
 
- #_(print (format-resolved-deps resolved-deps))
+  (pprint/pprint (format-resolved-deps resolved-deps))
 
-(pprint/pprint repositories)
+  #_(pprint/pprint repo-map-w-extras)
 
   )
